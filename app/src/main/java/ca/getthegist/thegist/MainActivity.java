1c398de0378b6e9cec28f16159d00881ea417bbc
SouthernYoda <107yoda@gmail.com>
2015-11-22 15:48:44 -0500

    The gist web view is done and displays the homepage in the app. The settings toolbar and banner are displaying.

--- app/src/main/java/ca/getthegist/thegist/MainActivity.java
+++ app/src/main/java/ca/getthegist/thegist/MainActivity.java
@@ -8,25 +8,40 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.webkit.WebView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        @Override
+        protected void onCreate(Bundle savedInstanceState)
+            {
+                super.onCreate(savedInstanceState);
+                setContentView(R.layout.activity_main);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-    }
+                String url="https://getthegist.ca/";
+                WebView view=(WebView)this.findViewById(R.id.webView);
+                view.getSettings().setJavaScriptEnabled(true);
+                view.loadUrl(url);
+
+
+
+                Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+                setSupportActionBar(toolbar);
+
+                FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+                fab.setOnClickListener(new View.OnClickListener()
+                                       {
+                    @Override
+                    public void onClick(View view)
+                    {
+                        Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                                .setAction("Action", null).show();
+                    }
+                }
+                )
+                ;
+        }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
